0a8d2c1bd9dc4856ac5e289cc66f697fa2e46692
roninfrodsham <roninfrodsham@gmail.com>
2022-06-15 22:29:44 +0100

    add AudioContext and use fetch get audio then decode

--- src/components/Boombox.tsx
+++ src/components/Boombox.tsx
@@ -6,7 +6,29 @@ import { TapeDeck } from "./tapedeck";
 import { TransportControls } from "./transportcontrols";
 import { VolumeIcon } from "./volumeicon";
 
+interface AudioBufferSourceNode {
+  buffer: AudioBuffer;
+}
+
 function BoomBox() {
+
+  const ctx = new AudioContext();
+  let audio: AudioBuffer;
+  fetch("./sounds/stereo_systems.mp3")
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+    .then(decodedAudio => {
+      audio = decodedAudio
+    })
+    .catch(error => console.log(error))
+  
+  function play() {
+    const source = ctx.createBufferSource();
+    source.buffer = audio;
+    source.connect(ctx.destination);
+    source.start(ctx.currentTime);
+  }
+
   return (
     <svg version="1.1" baseProfile="tiny" id="Layer_1" className="boombox" x="0px" y="0px" viewBox="0 0 800 477" overflow="visible">
       <BoomBody />
@@ -16,7 +38,7 @@ function BoomBox() {
       <Rotary xPos={150} />
       <PanIcon />
       <Rotary xPos={650} />
-      <TransportControls />
+      <TransportControls onClick={play} />
       <TapeDeck />
     </svg>    
   );
